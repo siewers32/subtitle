ee9715d6591c473f5f7b1adf67a6c59e3f0dd138
Jan Jaap <janjaap@siewers.org>
2024-02-03 23:15:24 +0100

    bugfixes

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def create_dictionaries(lines):
         fd = {}
         frame = lines[i[0]:i[1]]
         # t = addtime(frame[1], timediff)
+        print(frame)
         t = frame[1].split(" --> ") 
         fd["fnr"] = int(frame[0].strip("\n"))
         fd["start"] = t[0].strip("\n")
@@ -46,21 +47,21 @@ def adjust_time_abs(startframe, endframe, dictlist, timediff):
     return dictlist
 
 def adjust_time_rel(startframe, endframe, dictlist, perc):
-    for f in dictlist:
+    for i, f in enumerate(dictlist):
         if f["fnr"] > startframe and f["fnr"] <= endframe:
             # print(f["start"])
-            f["start"] = time_with_perc(perc, f["start"])[:-3]
-            f["end"] = time_with_perc(perc, f["end"])[:-3]
+            f["start"] = time_with_perc(perc, f["start"], perc)[:-3]
+            f["end"] = time_with_perc(perc, f["end"], perc)[:-3]
             print(f)
     return dictlist
 
 
-def time_with_perc(perc, hmsf):
+def time_with_perc(perc, hmsf, i):
     t0 = datetime.strptime("00:00:00,000", "%H:%M:%S,%f")
     t1 = datetime.strptime(hmsf, '%H:%M:%S,%f')
     delta = t1 - t0
     secs = delta.total_seconds()
-    total = (secs * (perc/100))
+    total = (secs * perc)/100
     s0 = timedelta(milliseconds=int(round((total * 1000),0)))
     d = s0 + t0
     return datetime.strftime(d, "%H:%M:%S,%f")
@@ -103,19 +104,27 @@ def calc_time_perc(ts1, ts2):
 # print(calc_time_difference('01:00:55','01:01:53'))
 # print(adjust_time_rel(1, 5, dictlist, "00:01:04,951", "00:02:00,00"))
 
-r = open("test.txt", "r") # subtitle file
+r = open("subtitle.srt", "r") # subtitle file
 
 lines = r.readlines()
 dictlist = create_dictionaries(lines)
 
-perc = calc_time_perc("01:29:52,178", "01:59:52,178")
 
 # adjust_time_rel(1, 5000, dictlist, perc)
 # adjust_time_abs(1, 5000, dictlist, timediff)
 
-timediff = timedelta(hours=0, minutes=2, seconds=58) # timediff
-new_sub_dict = adjust_time_abs(1, 5000, dictlist, timediff)
-# new_sub_dict = adjust_time_rel(1, 5000, dictlist, perc)
+timediff = timedelta(hours=0, minutes=0, seconds=2) # timediff
+# new_sub_dict = adjust_time_abs(90, 5000, dictlist, timediff)
+perc = calc_time_perc("01:13:54,830", "01:14:00,055")
+new_sub_dict = adjust_time_rel(0, 974, dictlist, perc)
+
+
+# 971
+# 01:13:51,464 --> 01:13:54,830
+# Yeah, I guess they are, it's a shame.
+
+
+
 
 w = open("blab.srt", "w")
 for d in new_sub_dict:
--- test.py
+++
@@ -1,4 +0,0 @@
-r = open("test.txt", "r") # subtitle file
-lines = r.readlines()
-for i in range(0, len(lines)):
-    print(i)
