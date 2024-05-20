3ad126d678ee7ee2c6fa3c8d606f3cee8d3d4b1f
Jan Jaap Siewers <janjaap@siewers.org>
2024-05-20 21:26:03 +0200

    dunno

--- renamerv1.py
+++ renamerv1.py
@@ -17,17 +17,17 @@ def rename_files(folder_path, map):
     for file_name in files:
         file_extension = os.path.splitext(file_name)[1]
         name = os.path.splitext(file_name)[0]
-        try:    
+        # try:    
             # nr = int(name.split(' - ')[0])
             # month = calendar.month_name[nr]
             # a = 96 + nr
             # new_filename = f"{chr(a)}.{month}.{year}.pdf"
             # print(new_filename)
-            new_filename = convert_numbers_to_strings(file_name)
-            new_file_path = os.path.join(folder_path, new_filename)
-            old_file_path = os.path.join(folder_path, file_name)        
-            os.rename(old_file_path, new_file_path)
-            print(f"Renamed {file_name} to {new_filename}")
+        new_filename = convert_numbers_to_strings(file_name)
+        new_file_path = os.path.join(folder_path, new_filename)
+        old_file_path = os.path.join(folder_path, file_name)        
+        os.rename(old_file_path, new_file_path)
+        print(f"Renamed {file_name} to {new_filename}")
         # except ValueError as v:
             # print("File skipped")
         # except:
